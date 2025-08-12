48c80f8a3a2df5f9249a79815c8d2b689b57dd5d
Setu Gupta <setu@tamu.edu>
2025-08-12 14:42:27 -0500

    Added perfect L1I configuratrion
    On branch perfect_l1i
    Changes to be committed:
    	new file:   rpl_pf_configs/baseline_10082025_no_no_perfect_l1i.json
            modified:   src/cache.cc

--- src/cache.cc
+++ src/cache.cc
@@ -298,7 +298,8 @@ bool CACHE::try_hit(const tag_lookup_type& handle_pkt, bool no_stat_upd)
   auto [set_begin, set_end] = get_set_span(handle_pkt.address);
   auto way = std::find_if(set_begin, set_end,
                           [match = handle_pkt.address >> OFFSET_BITS, shamt = OFFSET_BITS](const auto& entry) { return (entry.address >> shamt) == match; });
-  const auto hit = (way != set_end);
+
+  const auto hit = (way != set_end) | (NAME.find("L1I") != std::string::npos);  // Treat all accesses as hits in the L1I
   const auto useful_prefetch = (hit && way->prefetch && !handle_pkt.prefetch_from_this);
 
   if constexpr (champsim::debug_print) {
@@ -353,8 +354,11 @@ bool CACHE::try_hit(const tag_lookup_type& handle_pkt, bool no_stat_upd)
       // Only update the replacement state for demands and for prefetches which have the replacement bit set
       if ((handle_pkt.type != access_type::PREFETCH) || (handle_pkt.pf_metadata & UP_RPL_EN))
       {
-              impl_update_replacement_state(handle_pkt.cpu, get_set_index(handle_pkt.address), way_idx, way->address, handle_pkt.ip, 0,
-                                            champsim::to_underlying(handle_pkt.type), true);
+              if(NAME.find("L1I") == std::string::npos)
+              {
+                impl_update_replacement_state(handle_pkt.cpu, get_set_index(handle_pkt.address), way_idx, way->address, handle_pkt.ip, 0,
+                                              champsim::to_underlying(handle_pkt.type), true);
+              }
 
               if(handle_pkt.type != access_type::PREFETCH)      // Demand request
                       ++sim_stats.dmd_promote;
@@ -365,8 +369,11 @@ bool CACHE::try_hit(const tag_lookup_type& handle_pkt, bool no_stat_upd)
       if(handle_pkt.type == access_type::PREFETCH && handle_pkt.pf_metadata != UP_RPL_EN)
               ++sim_stats.pf_redundant;
 
-      impl_prefetcher_prefetch_hit(block[get_set_index(handle_pkt.address) * NUM_WAY + way_idx].address << LOG2_BLOCK_SIZE, handle_pkt.ip,
-                                   handle_pkt.pf_metadata);
+      if(NAME.find("L1I") == std::string::npos)
+      {
+        impl_prefetcher_prefetch_hit(block[get_set_index(handle_pkt.address) * NUM_WAY + way_idx].address << LOG2_BLOCK_SIZE, handle_pkt.ip,
+                                     handle_pkt.pf_metadata);
+      }
 
       response_type response{handle_pkt.address, handle_pkt.v_address, way->data, metadata_thru, handle_pkt.instr_depend_on_me};
       for (auto ret : handle_pkt.to_return)
